25d666d3278c79ddc205a195273eb97f41ee3d49
Zoltan Kakuk <sz.zoltan.kakuk@gmail.com>
2022-03-20 23:46:35 +0100

    Make DefaultRenderer works

--- page.js
+++ page.js
@@ -1,5 +1,26 @@
-let createNoteRenderer;
 const DELAY = 0;
+const VIDEO_NOTE_ROOT_ID = "videoNote-root";
+const buildPage = () => {
+  if (document.getElementById(VIDEO_NOTE_ROOT_ID)) {
+    return;
+  }
+
+  const videoNoteOverlay = document.createElement("div");
+  videoNoteOverlay.id = VIDEO_NOTE_ROOT_ID;
+  videoNoteOverlay.style.position = "fixed";
+  videoNoteOverlay.style.display = "block";
+  videoNoteOverlay.style.width = "100%";
+  videoNoteOverlay.style.height = "100%";
+  videoNoteOverlay.style.top = 0;
+  videoNoteOverlay.style.left = 0;
+  videoNoteOverlay.style.right = 0;
+  videoNoteOverlay.style.bottom = 0;
+  videoNoteOverlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+  videoNoteOverlay.style.zIndex = 100000;
+  videoNoteOverlay.style.cursor = "pointer";
+  document.body.appendChild(videoNoteOverlay);
+};
+let createNoteRenderer;
 
 (async () => {
   const module = await import(chrome.runtime.getURL("renderers/index.js"));
@@ -8,15 +29,33 @@ const DELAY = 0;
 
 chrome.runtime.onMessage.addListener(function ({ type }) {
   if (type === "LOAD_NOTE") {
+    // build page
+    buildPage();
     const video = document.querySelector("video");
     const noteUrl = chrome.runtime.getURL("notes/Friends.S01E01.json");
+    let isInitialized = false;
+
     // remove all existing tracks
     if (video) {
       for (let track of video.textTracks) {
-        track.mode = "disabled";
+        if (track.label === "VideoNotes") {
+          isInitialized = true;
+        } else {
+          track.mode = "disabled";
+        }
       }
+
+      if (isInitialized) {
+        document.getElementById(VIDEO_NOTE_ROOT_ID).style.display === "block"
+          ? (document.getElementById(VIDEO_NOTE_ROOT_ID).style.display = "none")
+          : (document.getElementById(VIDEO_NOTE_ROOT_ID).style.display =
+              "block");
+        return;
+      }
+
       // inject note as metadata track
       const track = video.addTextTrack("metadata", "VideoNotes");
+      track.id = "VideoNote";
       track.mode = "showing";
       track.addEventListener("cuechange", () => {
         const cue = track.activeCues[0];
--- renderers/DefaultRender.js
+++ renderers/DefaultRender.js
@@ -1,6 +1,21 @@
+const VIDEO_NOTE_ROOT_ID = "videoNote-root";
+
 export class DefaultRender {
   constructor(options) {}
-  renderNote(note) {
-    console.log(note);
+  renderNote({ content }) {
+    const videoNoteRoot = document.getElementById(VIDEO_NOTE_ROOT_ID);
+    const note = document.createElement("h1");
+    note.textContent = content;
+    note.style.color = "white";
+    note.style.position = "absolute";
+    note.style.top = "50%";
+    note.style.left = "50%";
+    note.style.fontSize = "50px";
+    note.style.transform = "translate(-50%,-50%)";
+    note.style.fontFamily = "sans-serif";
+    videoNoteRoot.childNodes.forEach((_, index) => {
+      videoNoteRoot.childNodes[index].remove();
+    });
+    videoNoteRoot.appendChild(note);
   }
 }
